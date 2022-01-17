54f5ba461cf17fef69e90ac8cee4eed6d5a1e8ac
coderkol95 <innocentangelfromhell@gmail.com>
2022-01-17 19:31:16 +0530

    datatypes finalised for real

--- auto_app.py
+++ auto_app.py
@@ -1,4 +1,3 @@
-from crypt import methods
 from flask import Flask, render_template, url_for, request, redirect
 import flask_excel as excel
 import pandas as pd
@@ -18,16 +17,22 @@ def dftolist(dataf):
         list_of_dict.append(internal_dict) 
     return list_of_dict
 
+def impute_data(nulls_n):
 
-def impute_data(imputation_type):
     global dataf
-    if imputation_type == 'row_removal':
-        dataf.dropna(inplace=True)
-    if imputation_type == 'mean':
-        dataf.fillna('mean')
 
+    for col, imp_typ in zip(nulls_n.column, nulls_n.imp_type):
+        if imp_typ == 'row_removal':
+            dataf = dataf[dataf[col].notnull()]
+        
+        print(dataf.loc[dataf.loc[dataf["Age"].isnull(), "Age"].index.tolist(), "Age"])
+        if imp_typ == 'mean':
+            dataf.loc[dataf[col].isnull(), col] = dataf[col].mean()
+        print(dataf.loc[dataf.loc[dataf["Age"].isnull(), "Age"].index.tolist(), "Age"])
 
 dataf=pd.DataFrame([])
+#Only for testing
+dataf = pd.read_csv(r'/Users/anupam/Downloads/titanic.csv')
 
 app = Flask(__name__)
 excel.init_excel(app)
@@ -44,38 +49,40 @@ def upload():
         dataf = pd.DataFrame(request.get_array(field_name='file'))
         dataf.columns = dataf.iloc[0,:]
         dataf = dataf.iloc[1:,:]
+
         return redirect(url_for("datatypes"))
     return render_template("upload.html")
 
 @app.route('/datatypes', methods=["GET","POST"])
 def datatypes():
     global dataf
-    #Only for testing
-    dataf = pd.read_csv(r'/Users/anupam/Downloads/Telco-Customer-Churn.csv')
+
     column_names = dataf.columns.astype('str').tolist()
     data_types = dataf.dtypes.astype('str').tolist()
     datatypes = pd.DataFrame({'column':column_names,'datatype': data_types})
+    datatypes['datatype'].replace({'object':'string'}, inplace=True)
     datadict = dftolist(datatypes)
 
     # print(datadict)
     if request.method == "POST":
         data_types = []
-        data_types.append(request.form.getlist("datatype_from_form"))
-        data_types = pd.Series(data_types[0]).replace({'object':'str'}).tolist()
-        # print(data_types)
+        data_types = request.form.getlist("datatype_from_form")
+        print(data_types)
     
         for var, input_type in zip(column_names, data_types):
-           dataf[var] = dataf[var].astype(input_type)
-        
-        # print(dataf.head())
-        # print(dataf.dtypes)
-        
+            if input_type=='str': 
+                dataf[var] = dataf[var].astype(input_type)
+            else:
+                dataf[var].replace({'':np.nan}, inplace=True) 
+                dataf[var] = dataf[var].astype(input_type)
         return redirect(url_for('preprocessing'))
+    print(dataf.head())
 
     return render_template('datatypes.html', data=datadict)
 
 @app.route("/preprocessing", methods=["GET","POST"])
 def preprocessing():
+    print(dataf.dtypes)
     return render_template("preprocessing.html")
 
 @app.route("/impute", methods=["GET","POST"])
@@ -89,15 +96,21 @@ def impute():
 
     dataf.replace({'?':np.nan, 'null':np.nan, 'Null':np.nan, 'NULL':np.nan, '':np.nan}, inplace=True)
     nulls = dataf.isnull().sum().to_frame()
-    nulls.reset_index(inplace=True)
-    nulls.columns = ["column","null_count"]
-    nulls_n = nulls[nulls.null_count>0]
+    nulls_n = nulls[nulls[0]>0]
+    nulls_n.columns=['null_count']
+    
+    nulls_n.reset_index(inplace=True)
+    print(nulls_n)
+
+    nulls_n.rename( columns = {0:"column"}, inplace=True)
     null_count_frame = dftolist(nulls_n)
 
     if request.method == "POST":
         imputation_type = request.form.getlist("imputetype")
-        print(imputation_type)
-        impute_data(imputation_type)
+        nulls_n['imp_type'] = pd.Series(imputation_type)
+        print(nulls_n)
+
+        impute_data(nulls_n[['column','imp_type']])
         return redirect(url_for("preprocessing"))
     return render_template("impute.html", data = null_count_frame)
 
